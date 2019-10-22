210afceca3a29f2bcc3325f47e5104a996d1686d
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-22 11:05:30 +0300

    list of people

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -9,22 +9,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let people = ["Finn", "Leila", "Luke", "Rey"]
+    
     var body: some View {
-        List {
-            Section(header: Text("Section 1:")) {
-                Text("Static row 1")
-                Text("Static row 2")
-            }
-            Section(header: Text("Section 2:")) {
-                ForEach(0..<5) {
-                    Text("Dynamic row \($0)")
-                }
-            }
-            Section(header: Text("Section 3:")) {
-                Text("Static row 3")
-                Text("Static row 4")
-            }
-        }
+        List(people, id: \.self) {
+            Text($0)
+        }.listStyle(GroupedListStyle())
     }
 }
 
