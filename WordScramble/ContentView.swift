3e546ea557d7e77cc4763d865aea0e33abf8bb84
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-22 11:00:47 +0300

    sections

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -11,9 +11,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         List {
-            Text("Hello World")
-            Text("Hello World")
-            Text("Hello World")
+            Section(header: Text("Section 1:")) {
+                Text("Static row 1")
+                Text("Static row 2")
+            }
+            Section(header: Text("Section 2:")) {
+                ForEach(0..<5) {
+                    Text("Dynamic row \($0)")
+                }
+            }
+            Section(header: Text("Section 3:")) {
+                Text("Static row 3")
+                Text("Static row 4")
+            }
         }
     }
 }
